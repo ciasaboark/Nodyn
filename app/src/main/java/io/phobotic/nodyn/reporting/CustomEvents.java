c8a3f0d4590cf9460fa3cd056c4f4c9361595a43
Jonathan Nelson <ciasaboark@gmail.com>
2017-10-19 12:22:34 -0400

    Adding additional events for app usage tracking

--- app/src/main/java/io/phobotic/nodyn/reporting/CustomEvents.java
+++ app/src/main/java/io/phobotic/nodyn/reporting/CustomEvents.java
@@ -22,8 +22,18 @@ package io.phobotic.nodyn.reporting;
  */
 
 public class CustomEvents {
+    public static final String SYNC_SCHEDULED = "Sync scheduled";
     public static final String SYNC_SUCCESS = "Sync Success";
     public static final String SYNC_ERROR_ACTION_FAILED = "Sync Error: Action failed";
     public static final String SYNC_ERROR_EMAIL_SENT = "Sync Error: Email sent";
     public static final String SYNC_ERROR_EMAIL_NOT_SENT = "Sync Error: Email not sent";
+    public static final String UPGRADE_RECEIVER_FIRED = "Upgrade Receiver called";
+    public static final String BOOT_RECEIVER_FIRED = "Boot Receiver called";
+    public static final String SYNC_FAILED = "Sync failed";
+    public static final String ASSET_PAST_DUE = "Asset Past Due";
+
+
+    public static final String EXCEPTION_NAME = "exception name";
+    public static final String PAST_DUE_EMAIL_NOT_SENT = "Asset past due email not sent";
+    public static final String PAST_DUE_EMAIL_SENT = "Asset past due email sent";
 }
